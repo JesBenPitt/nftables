288ef834a22356e2a10f1a584d6c484c294811f3
Jesús Benítez Pitt <148717088+JesBenPitt@users.noreply.github.com>
2024-02-08 23:36:52 +0100

    Update funcionamiento_dmz_fachada.sh

--- funcionamiento_dmz_fachada.sh
+++ funcionamiento_dmz_fachada.sh
@@ -1,9 +1,16 @@
-echo "Comprobando ping a DMZ desde FACHADA."
+echo "Comprobando ping a FACHADA desde DMZ."
 ping -c1 192.168.111.1
 if [ $? == 0 ]
   then
-    echo "Ping a LAN desde FACHADA funcionando correctamente."
+    echo "Ping a FACHADA desde DMZ funcionando correctamente."
   else
-    echo "Ha ocurrido un error con el ping a LAN."
+    echo "Ha ocurrido un error con el ping a FACHADA."
 fi
-echo "Comprobando consultas DNS desde FACHADA."
+echo "Comprobando consultas DNS desde DMZ a través de FACHADA."
+nslookup google.com
+if [ $? == 0 ]
+  then
+    echo "Consultas DNS desde DMZ a través de FACHADA funcionando correctamente."
+  else
+    echo "Ha ocurrido un error con las consultas a DNS desde DMZ a través de FACHADA."
+fi
