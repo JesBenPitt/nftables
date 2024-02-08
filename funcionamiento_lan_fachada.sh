4913927e7c4b7df52e46e2eaecf948b7683fb175
Jesús Benítez Pitt <148717088+JesBenPitt@users.noreply.github.com>
2024-02-08 23:49:29 +0100

    Update funcionamiento_lan_fachada.sh

--- funcionamiento_lan_fachada.sh
+++ funcionamiento_lan_fachada.sh
@@ -4,27 +4,27 @@ rojo="\e[31m"
 verde="\e[32m"
 amarillo="\e[33m"
 fin="\e[0m"
-echo "${amarillo}Comprobando ping a FACHADA desde LAN.${fin}"
+echo -e "${amarillo}Comprobando ping a FACHADA desde LAN.${fin}"
 ping -c1 192.168.33.1
 if [ $? == 0 ]
   then
-    echo "${verde}Ping a FACHADA desde LAN funcionando correctamente.${fin}"
+    echo -e "${verde}Ping a FACHADA desde LAN funcionando correctamente.${fin}"
   else
-    echo "${rojo}Ha ocurrido un error con el ping a FACHADA.${fin}"
+    echo -e "${rojo}Ha ocurrido un error con el ping a FACHADA.${fin}"
 fi
-echo "${amarillo}Comprobando consultas DNS desde LAN a través de FACHADA.${fin}"
+echo -e "${amarillo}Comprobando consultas DNS desde LAN a través de FACHADA.${fin}"
 nslookup google.com
 if [ $? == 0 ]
   then
-    echo "${verde}Consultas DNS desde LAN a través de FACHADA funcionando correctamente.${fin}"
+    echo -e "${verde}Consultas DNS desde LAN a través de FACHADA funcionando correctamente.${fin}"
   else
-    echo "${rojo}Ha ocurrido un error con las consultas a DNS desde LAN a través de FACHADA.${fin}"
+    echo -e "${rojo}Ha ocurrido un error con las consultas a DNS desde LAN a través de FACHADA.${fin}"
 fi
-echo "${amarillo}Comprobando ping a DMZ desde LAN.${fin}"
+echo -e "${amarillo}Comprobando ping a DMZ desde LAN.${fin}"
 ping -c1 192.168.111.2
 if [ $? == 0 ]
   then
-    echo "${verde}Ping a DMZ desde LAN funcionando correctamente.${fin}"
+    echo -e "${verde}Ping a DMZ desde LAN funcionando correctamente.${fin}"
   else
-    echo "${rojo}Ha ocurrido un error con el ping a DMZ.${fin}"
+    echo -e "${rojo}Ha ocurrido un error con el ping a DMZ.${fin}"
 fi
