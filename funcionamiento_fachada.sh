47fe9356437395e2670ffcc1d4fa6e2510c37ecb
Jesús Benítez Pitt <148717088+JesBenPitt@users.noreply.github.com>
2024-02-08 23:50:37 +0100

    Update funcionamiento_fachada.sh

--- funcionamiento_fachada.sh
+++ funcionamiento_fachada.sh
@@ -2,19 +2,19 @@ rojo="\e[31m"
 verde="\e[32m"
 amarillo="\e[33m"
 fin="\e[0m"
-echo "${amarillo}Comprobando Ping a Google desde fachada.${fin}"
+echo -e "${amarillo}Comprobando Ping a Google desde fachada.${fin}"
 ping -c1 8.8.8.8
 if [ $? == 0 ]
   then
-    echo "${verde}Ping a Google desde fachada funcionando correctamente.${fin}"
+    echo -e "${verde}Ping a Google desde fachada funcionando correctamente.${fin}"
   else
-    echo "${rojo}Ha ocurrido un error con el ping a Google.${fin}"
+    echo -e "${rojo}Ha ocurrido un error con el ping a Google.${fin}"
 fi
-echo "${amarillo}Comprobando consultas DNS desde FACHADA.${fin}"
+echo -e "${amarillo}Comprobando consultas DNS desde FACHADA.${fin}"
 nslookup google.com
 if [ $? == 0 ]
   then
-    echo "${verde}Consultas DNS desde FACHADA funcionando correctamente.${fin}"
+    echo -e "${verde}Consultas DNS desde FACHADA funcionando correctamente.${fin}"
   else
-    echo "${rojo}Ha ocurrido un error con las consultas a DNS desde FACHADA.${fin}"
+    echo -e "${rojo}Ha ocurrido un error con las consultas a DNS desde FACHADA.${fin}"
 fi
