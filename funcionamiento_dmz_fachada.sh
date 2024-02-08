fb376df7ee8b7f623d4b740efa94feb165bbc0c0
Jesús Benítez Pitt <148717088+JesBenPitt@users.noreply.github.com>
2024-02-08 23:43:26 +0100

    Update funcionamiento_dmz_fachada.sh

--- funcionamiento_dmz_fachada.sh
+++ funcionamiento_dmz_fachada.sh
@@ -1,26 +1,30 @@
 sudo nft flush ruleset
 
-echo "Comprobando ping a FACHADA desde DMZ."
+rojo="\e[31m"
+verde="\e[32m"
+amarillo="\e[33m"
+fin="\e[0m"
+echo "${amarillo}Comprobando ping a FACHADA desde DMZ.${fin}"
 ping -c1 192.168.111.1
 if [ $? == 0 ]
   then
-    echo "Ping a FACHADA desde DMZ funcionando correctamente."
+    echo "${verde}Ping a FACHADA desde DMZ funcionando correctamente.${fin}"
   else
-    echo "Ha ocurrido un error con el ping a FACHADA."
+    echo "${rojo}Ha ocurrido un error con el ping a FACHADA.${fin}"
 fi
-echo "Comprobando consultas DNS desde DMZ a través de FACHADA."
+echo "${amarillo}Comprobando consultas DNS desde DMZ a través de FACHADA.${fin}"
 nslookup google.com
 if [ $? == 0 ]
   then
-    echo "Consultas DNS desde DMZ a través de FACHADA funcionando correctamente."
+    echo "${verde}Consultas DNS desde DMZ a través de FACHADA funcionando correctamente.${fin}"
   else
-    echo "Ha ocurrido un error con las consultas a DNS desde DMZ a través de FACHADA."
+    echo "${rojo}Ha ocurrido un error con las consultas a DNS desde DMZ a través de FACHADA.${fin}"
 fi
-echo "Comprobando ping a LAN desde DMZ."
+echo "${amarillo}Comprobando ping a LAN desde DMZ.${fin}"
 ping -c1 192.168.33.1
 if [ $? == 0 ]
   then
-    echo "Ping a LAn desde DMZ funcionando correctamente."
+    echo "${verde}Ping a LAn desde DMZ funcionando correctamente.${fin}"
   else
-    echo "Ha ocurrido un error con el ping a LAN."
+    echo "${rojo}Ha ocurrido un error con el ping a LAN.${fin}"
 fi
