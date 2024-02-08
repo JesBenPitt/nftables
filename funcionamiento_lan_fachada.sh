b9fc624bfd73541709c819a3ac25348f95322b58
Jesús Benítez Pitt <148717088+JesBenPitt@users.noreply.github.com>
2024-02-08 23:40:03 +0100

    Update funcionamiento_lan_fachada.sh

--- funcionamiento_lan_fachada.sh
+++ funcionamiento_lan_fachada.sh
@@ -1,3 +1,5 @@
+sudo nft flush ruleset
+
 echo "Comprobando ping a FACHADA desde LAN."
 ping -c1 192.168.33.1
 if [ $? == 0 ]
@@ -13,4 +15,12 @@ if [ $? == 0 ]
     echo "Consultas DNS desde LAN a través de FACHADA funcionando correctamente."
   else
     echo "Ha ocurrido un error con las consultas a DNS desde LAN a través de FACHADA."
+fi
+echo "Comprobando ping a DMZ desde LAN."
+ping -c1 192.168.111.2
+if [ $? == 0 ]
+  then
+    echo "Ping a DMZ desde LAN funcionando correctamente."
+  else
+    echo "Ha ocurrido un error con el ping a DMZ."
 fi
