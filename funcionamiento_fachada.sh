d3b7f521b8df4c106b887530a9c562f462e0c05b
Jesús Benítez Pitt <148717088+JesBenPitt@users.noreply.github.com>
2024-02-08 23:32:31 +0100

    Update funcionamiento_fachada.sh

--- funcionamiento_fachada.sh
+++ funcionamiento_fachada.sh
@@ -6,23 +6,6 @@ if [ $? == 0 ]
   else
     echo "Ha ocurrido un error con el ping a Google."
 fi
-echo "Comprobando ping a LAN desde FACHADA."
-ping -c1 192.168.33.1
-if [ $? == 0 ]
-  then
-    echo "Ping a LAN desde FACHADA funcionando correctamente."
-  else
-    echo "Ha ocurrido un error con el ping a LAN."
-fi
-echo "Comprobando ping a DMZ desde FACHADA."
-ping -c1 192.168.111.1
-if [ $? == 0 ]
-  then
-    echo "Ping a LAN desde FACHADA funcionando correctamente."
-  else
-    echo "Ha ocurrido un error con el ping a LAN."
-fi
-echo "Comprobando consultas DNS desde FACHADA."
 nslookup google.com
 if [ $? == 0 ]
   then
