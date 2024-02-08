c967c333fb997277150cbe58a9580f1f922d2dad
Jesús Benítez Pitt <148717088+JesBenPitt@users.noreply.github.com>
2024-02-08 23:36:45 +0100

    Update funcionamiento_lan_fachada.sh

--- funcionamiento_lan_fachada.sh
+++ funcionamiento_lan_fachada.sh
@@ -1,1 +1,16 @@
-
+echo "Comprobando ping a FACHADA desde LAN."
+ping -c1 192.168.33.1
+if [ $? == 0 ]
+  then
+    echo "Ping a FACHADA desde LAN funcionando correctamente."
+  else
+    echo "Ha ocurrido un error con el ping a FACHADA."
+fi
+echo "Comprobando consultas DNS desde LAN a través de FACHADA."
+nslookup google.com
+if [ $? == 0 ]
+  then
+    echo "Consultas DNS desde LAN a través de FACHADA funcionando correctamente."
+  else
+    echo "Ha ocurrido un error con las consultas a DNS desde LAN a través de FACHADA."
+fi
